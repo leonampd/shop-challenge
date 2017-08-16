7fb3325a6b245eb19a2ee36db4987e6aa938403c
Leonam Pereira Dias <leonam.pd@gmail.com>
2017-08-16 16:54:15 -0300

    validando se o pagamento deve ser dividido entre vendedores

--- application/src/Leonam/ShopChallenge/Bundle/Tests/Entity/Purchase/PurchaseTest.php
+++ application/src/Leonam/ShopChallenge/Bundle/Tests/Entity/Purchase/PurchaseTest.php
@@ -95,30 +95,23 @@ class PurchaseTest extends TestCase
         $this->assertFalse($purchase->paymentShouldBeSplited());
     }
 
-//    public function testIfThePaymentShouldBeDividedForDifferentSellers()
-//    {
-//        $this->markTestSkipped();
-//        $seller1 = new Seller('seller 1');
-//        $seller1->setRecipientId('123');
-//
-//        $product1 = new Product('product', 125, $seller1);
-//        $purchaseItem = new PurchaseItem($product1,1);
-//
-//        $product2 = new Product('product 2', 125, $seller1);
-//        $purchaseItem2 = new PurchaseItem($product2,1);
-//
-//        $purchase = new Purchase();
-//        $purchase->setItems([$purchaseItem, $purchaseItem2]);
-//
-//        $this->assertFalse($purchase->paymentShouldBeSplited());
-//
-//        $differentSeller = new Seller('a different seller');
-//        $differentSeller->setRecipientId('987');
-//
-//        $purchaseItem3 = clone $purchaseItem;
-//        $purchaseItem3->getProduct()->setSeller( $differentSeller );
-//
-//        $purchase->addItem($purchaseItem3);
-//        $this->assertTrue($purchase->paymentShouldBeSplited());
-//    }
+    public function testIfThePaymentShouldBeDividedForDifferentSellers()
+    {
+        $seller1 = new Seller('seller 1', Seller::MARKETPLACE_OWNER);
+        $seller1->setRecipientId('123');
+
+        $seller2 = clone $seller1;
+        $seller2->setRecipientId('321');
+
+        $product1 = new Product('product', 125, $seller1);
+        $purchaseItem = new PurchaseItem($product1,1);
+
+        $product2 = new Product('product 2', 125, $seller2);
+        $purchaseItem2 = new PurchaseItem($product2,1);
+
+        $purchase = new Purchase();
+        $purchase->setItems([$purchaseItem, $purchaseItem2]);
+
+        $this->assertTrue($purchase->paymentShouldBeSplited());
+    }
 }
