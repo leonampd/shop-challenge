5dc0bae4cb728a2a28d3071ddc62cc6a7fc23966
Leonam Pereira Dias <leonam.pd@gmail.com>
2017-08-16 00:39:01 -0300

    Recuperando card_hash da api via JS e executando submit do form no endpoint

--- application/web/assets/js/application.js
+++ application/web/assets/js/application.js
@@ -1,30 +1,35 @@
-$(document).ready(function() {
-  var form = $("#payment_form")
-
-  form.submit(function(event) {
-    event.preventDefault();
+var getCreditCardDatav = function () {
     var card = {}
-    card.card_holder_name = $("#card_holder_name").val()
-    card.card_expiration_date = $("#card_expiration_month").val() + '/' + $("#card_expiration_year").val()
-    card.card_number = $("#card_number").val()
-    card.card_cvv = $("#card_cvv").val()
+    card.card_holder_name = jQuery("#card_holder_name").val()
+    card.card_expiration_date = jQuery("#card_expiration_month").val() + '/' + jQuery("#card_expiration_year").val()
+    card.card_number = jQuery("#card_number").val()
+    card.card_cvv = jQuery("#card_cvv").val()
 
-    // pega os erros de validação nos campos do form e a bandeira do cartão
-    var cardValidations = pagarme.validate({card: card})
-
-    //Então você pode verificar se algum campo não é válido
-    if(!cardValidations.card.card_number) {
-      console.log('Oops, número de cartão incorreto')
-    }
-
-    //Mas caso esteja tudo certo, você pode seguir o fluxo
+    return card;
+};
+var getCardHashFromPagarme = function(card, callback) {
     pagarme.client.connect({ encryption_key: 'ek_test_pvXfMOPeMo0eN7A6xLMASyuqETWA1Q' })
-      .then(client => client.security.encrypt(card))
-  .then(card_hash => console.log(card_hash))
-    event.
-    // o próximo passo aqui é enviar o card_hash para seu servidor, e
-    // em seguida criar a transação/assinatura
+        .then(client => client.security.encrypt(card))
+        .then(card_hash => callback(card_hash));
+};
 
-    return false
-  })
+jQuery(document).ready(function () {
+    var btnFinalizarCompra = jQuery('.btn-finalizar-compra');
+
+    btnFinalizarCompra.on('click', function () {
+        var card = getCreditCardDatav();
+        var cardValidations = pagarme.validate({card: card})
+        if(!cardValidations.card.card_number){
+            jQuery('#field_errors').text('Oops, número de cartão incorreto');
+            return;
+        }
+
+        getCardHashFromPagarme(card, function (card_hash) {
+            jQuery('#card_hash').val(card_hash);
+            if (card_hash !== '') {
+                jQuery('#payment_form').submit();
+            }
+        });
+
+    });
 });
