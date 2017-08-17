e9abed121254ff13268aa7a0280f43dd6b7a075c
Leonam Pereira Dias <leonam.pd@gmail.com>
2017-08-17 18:44:25 -0300

    Removendo teste de regra simples e definindo "outro" vendedor pro produto

--- application/src/Leonam/ShopChallenge/Bundle/Tests/CalculationRule/Purchase/MariaMarketPlaceCalculationRuleTest.php
+++ application/src/Leonam/ShopChallenge/Bundle/Tests/CalculationRule/Purchase/MariaMarketPlaceCalculationRuleTest.php
@@ -17,30 +17,6 @@ use PHPUnit\Framework\TestCase;
 class MariaMarketPlaceCalculationRuleTest extends TestCase
 {
     /**
-     * @covers \Leonam\ShopChallenge\Bundle\CalculationRule\Purchase\SimpleTotalCalculation::calculate()
-     * @covers \Leonam\ShopChallenge\Bundle\Entity\Purchase\Purchase::getTotal()
-     */
-    public function testIfNoRuleSettedTotalPurchaseShouldBySumOfItemPricesMultipliedByQuantity()
-    {
-        $seller = new Seller('seller 1');
-        $product1 = new Product('product', 100, $seller);
-        $purchaseItem = new PurchaseItem($product1,2);
-
-        $purchase = new Purchase();
-        $purchase->setItems([$purchaseItem]);
-
-        $this->assertEquals(200, $purchase->getTotal());
-
-        $product2 = clone $product1;
-        $product2->setValue(250);
-        $purchaseItem2 = new PurchaseItem($product2, 1);
-
-        $purchase->addItem($purchaseItem2);
-
-        $this->assertEquals(450, $purchase->getTotal());
-    }
-
-    /**
      * @covers \Leonam\ShopChallenge\Bundle\CalculationRule\Purchase\MariaMarketPlaceCalculationRule::calculate()
      * @covers \Leonam\ShopChallenge\Bundle\Entity\Purchase\Purchase::getTotal()
      */
@@ -58,7 +34,9 @@ class MariaMarketPlaceCalculationRuleTest extends TestCase
 
         $this->assertEquals(16700, $purchase->getTotal());
 
+        $anotherSeller = new Seller('another seller');
         $product2 = clone $product1;
+        $product2->setSeller($anotherSeller);
         $product2->setValue(100);
         $purchaseItem2 = new PurchaseItem($product2, 1);
 
